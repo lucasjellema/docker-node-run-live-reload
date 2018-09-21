fd8c5373186c8fabcbb7077fff3640001938dd23
Lucas Jellema <lucas.jellema@amis.nl>
2018-09-21 09:46:16 +0200

    Finetuning startUpScript.sh

--- startUpScript.sh
+++ startUpScript.sh
@@ -3,27 +3,25 @@ CONTAINER_ALREADY_STARTED="CONTAINER_ALREADY_STARTED_PLACEHOLDER"
 if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
     touch $CONTAINER_ALREADY_STARTED
     echo "-- First container startup --"
-    # YOUR_JUST_ONCE_LOGIC_HERE
+    # this branch is only executed when the container is first started
     cd /tmp
-    # install dependencies for reload app
-    # npm install
     # prepare the actual Node app from GitHub
     mkdir app
     git clone $GITHUB_URL app
     cd app
     #install dependencies for the Node app
     npm install
-    #start  both the reload app and (using nodemon) the actual Node app
+    #start  both the reload app (in the background) and (using nodemon) the actual Node app
     cd ..
     echo "starting reload app and nodemon"
-    (echo "starting reload app") & (echo "start reload";npm start; echo "reload app finished") & 
+    (echo "start reload";npm start; echo "reload app finished") & 
     cd app; 
     echo "starting nodemon for app cloned from $GITHUB_URL";
     nodemon
 else
     echo "-- Not first container startup --"
     cd /tmp
-    (echo "starting reload app and nodemon") & (echo "start reload";npm start; echo "reload app finished") & (cd app; echo "start nodemon") &
+    (echo "start reload";npm start; echo "reload app finished") &
     cd app; 
     echo "starting nodemon for app cloned from $GITHUB_URL";
     nodemon
