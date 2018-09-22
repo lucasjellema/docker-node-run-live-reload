ca5eba6f5d2a87f7ae7897cf1e6059b0bd29c1c9
Lucas Jellema <lucas.jellema@amis.nl>
2018-09-22 17:13:04 +0200

    Use explicit directory names in startUpScript

--- startUpScript.sh
+++ startUpScript.sh
@@ -9,22 +9,23 @@ if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
     # prepare the actual Node app from GitHub
     mkdir app
     git clone $GITHUB_URL app
-    cd app/$TARGET_DIR
+    echo 'GIT repo was cloned to /tmp/app/$TARGET_DIR'
+    cd /tmp/app/$TARGET_DIR
     #install dependencies for the Node app
     npm install
     #start  both the reload app (in the background) and (using nodemon) the actual Node app
-    cd ..
+    cd /tmp
     echo "starting reload app and nodemon"
     (echo "start reload";npm start; echo "reload app finished") & 
-    cd app/$TARGET_DIR; 
-    echo "starting nodemon for app cloned from $GITHUB_URL";
+    cd /tmp/app/$TARGET_DIR; 
+    echo "starting nodemon for app cloned from $GITHUB_URL in /tmp/app/$TARGET_DIR";
     nodemon
 else
     echo "-- Not first container startup --"
     cd /tmp
     (echo "start reload";npm start; echo "reload app finished") &
-    cd app/$TARGET_DIR; 
-    echo "starting nodemon for app cloned from $GITHUB_URL";
+    cd /tmp/app/$TARGET_DIR; 
+    echo "starting nodemon for app cloned from $GITHUB_URL in directory /tmp/app/$TARGET_DIR";
     nodemon
 fi
 
