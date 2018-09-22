09c3d106151315521bcd7759f621ebc3bfd0fddc
Lucas Jellema <lucas.jellema@amis.nl>
2018-09-22 13:28:02 +0200

    Support GitHub WebHook handling

--- gitRefresh.sh
+++ gitRefresh.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-    cd /tmp/app
-    git pull 
-    #install dependencies for the Node app
-    npm install
+cd /tmp/app
+git pull 
+#install dependencies for the Node app
+npm install
 
--- server.js
+++ server.js
@@ -1,23 +1,62 @@
     /* this program listens for /reload request at port 4500. 
+    or a GitHub WebHook trigger (see: https://technology.amis.nl/2018/03/20/handle-a-github-push-event-from-a-web-hook-trigger-in-a-node-application/)
     When it receives such a request, it will perform a Git pull in the app sub directory (from where this application runs) 
 
     TODO
     - add the option to schedule an automatic periodic git pull
-    - expose an endpoint for a GitHub WebHook signal, to respond to a Git commit
 
     - use https://www.npmjs.com/package/simple-git instead of shelljs plus local Git client (this could allow usage of a lighter base image - e.g. node-slim)
     */
 
     const RELOAD_PATH = '/reload'
-    const APP_DIRECTORY = '\\app'
+    const GITHUB_WEBHOOK_PATH = '/github/push'
 
     var http = require('http');
     var server = http.createServer(function (request, response) {
-        console.log('request starting...');
         if (request.method === 'GET' && request.url === RELOAD_PATH) {
+            console.log(`reload request starting at ${new Date().toISOString()}...`);
             refreshAppFromGit();
             response.write(`RELOADED!!${new Date().toISOString()}`);
             response.end();
+            console.log('reload request handled...');
+        } if (request.method === 'POST' && request.url === GITHUB_WEBHOOK_PATH) {
+            console.log(`GitHub WebHook event handling starting ${new Date().toISOString()}...`);
+                var githubEvent = request.body
+                // - githubEvent.head_commit is the last (and frequently the only) commit
+                // - githubEvent.pusher is the user of the pusher pusher.name and pusher.email
+                // - timestamp of final commit: githubEvent.head_commit.timestamp
+                // - branch:  githubEvent.ref (refs/heads/master)
+               
+                var commits = {}
+                if (githubEvent.commits)
+                  commits = githubEvent.commits.reduce(
+                    function (agg, commit) {
+                      agg.messages = agg.messages + commit.message + ";"
+                      agg.filesTouched = agg.filesTouched.concat(commit.added).concat(commit.modified).concat(commit.removed)
+//                        .filter(file => file.indexOf("src/js/jet-composites/input-country") > -1)
+                      return agg
+                    }
+                    , { "messages": "", "filesTouched": [] })
+               
+                var push = {
+                  "finalCommitIdentifier": githubEvent.after,
+                  "pusher": githubEvent.pusher,
+                  "timestamp": githubEvent.head_commit.timestamp,
+                  "branch": githubEvent.ref,
+                  "finalComment": githubEvent.head_commit.message,
+                  "commits": commits
+                }
+                console.log("WebHook Push Event: " + JSON.stringify(push))
+                if (push.commits.filesTouched.length > 0) {
+                  console.log("This commit involves changes to the Node application, so let's perform a git pull ")
+                  refreshAppFromGit();
+                }
+               
+                var response = push
+                response.json(response)
+                console.log(`GitHub WebHook event handling complete at ${new Date().toISOString()}`);
+              
+
         }
         else {
             // respond
